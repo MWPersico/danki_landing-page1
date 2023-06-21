b2c20835f0e8aec4dd4d4885a9690fb49f89aa95
Marcos Willian <willian.marcos7@hotmail.com>
2023-06-21 18:28:18 -0300

    slide adicionado

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,6 @@
-// Alerta - Links:
-var links = document.querySelectorAll('.link-item');
+$(function(){
+    // Alerta - Links:
+var links = $('.link-tiem');
 for(element of links){
     if(element.innerText != "home")element.addEventListener('click', alertLinks)
 }
@@ -23,3 +24,38 @@ function showMenu(){
     burguer.classList.toggle('active');
     menu.classList.toggle('show');
 }
+
+initSlide();
+
+function initSlide(){
+    let delay = 4000;
+    let slideCount = $(".scroller-item").length;
+    
+    for(let i = 0; i<slideCount; i++){
+        if(i!=0) $("#slide-bullets").append(`<span class='bullet' count='${i}'></span>`);
+        else $("#slide-bullets").append(`<span class='bullet active' count='${i}'></span>`);
+    }
+    slideAutoPlay(delay, slideCount);
+}
+    
+function slideAutoPlay(delay, slideCount){
+    let currentIndex = 1;
+    setInterval(function(){  
+        if(currentIndex == slideCount) currentIndex = 0;
+        changeSlide(currentIndex);
+        $(".bullet.active").removeClass('active');
+        $(".bullet").eq(currentIndex).addClass('active');
+        currentIndex++;
+    }, delay)
+}
+
+function changeSlide(currentIndex){
+    var offsetX = $(".scroller-item").eq(currentIndex).offset().left - $("#team-scroller").offset().left;
+    if(currentIndex > 1)offsetX= offsetX*currentIndex;
+    $("#team-scroller").stop().animate({'scrollLeft':offsetX})
+}
+
+$(window).resize(function(){
+    $("#team-scroller").stop().animate({'scrollLeft':0})
+})
+})
